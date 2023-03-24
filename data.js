772f5aa77b1b0ede6cd974ee8ecf1fe60cf2388f
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-03-24 11:39:00 -0500

    Updates

--- data.js
+++ data.js
@@ -1,9 +1,29 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const filterData = (dataList, condition) => {
+  const dataFilter = dataList.filter((item) => item.house === condition);
+  return dataFilter;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const filterProtagonists = (datalist, protagonists) => {
+  const dataFilter = datalist.filter((item) => protagonists.includes(item.name));
+  return dataFilter;
+};
+
+export const sortList = (dataList) => {
+  return dataList.concat().sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0
+  });
+};
+
+export const searchFilter = (dataList, condition) => {
+  const dataFilter = dataList.filter( item => item.name.toLowerCase().includes(condition));
+  return dataFilter;
+};
+
+export const calculate = (number) => {
+  const percentage = (number/50) * 100;
+  return percentage.toFixed()
 };
--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-import { apiHarry } from './api/apiHarry.js';
+//import { apiHarry } from './api/apiHarry.js';
+import { filterData, filterProtagonists, searchFilter, sortList, calculate } from './data.js';
 import listCH from './data/harrypotter/CharactersList.js';
 
+const slSort = document.querySelector('#sl-sort');
 const listElement = document.getElementById("list");
 listElement.style.display = 'none';
 const loadingDiv = document.getElementById("loading")
 let dataList;
+let dataFilter;
 const charactersName = document.getElementById("characters-names");
 const navMenu = document.querySelector('.Menu');
-const navToogle = document.querySelector('.toggle')
+const navToogle = document.querySelector('.toggle');
+
+const apiHarry = async () => {
+  const response = await fetch("data/harrypotter/harry.json");
+  return await response.json()
+}
+
 apiHarry().then((listHarry) => {
   loadingDiv.style.display = 'none';
   listElement.style.display = 'block';
-  dataList = listHarry 
-  renderList(listHarry.characters)
-  
-})
+  dataList = listHarry;
+  dataFilter = listHarry.characters;
+  renderProtagonists();
+});
+
+function listCharacters(listHarry) {
+  const newList = []
+  listHarry.map((item, index) => {
+    const validate = listCH.charactersList.find((itemCH) => itemCH.name === item.name);
+    if (validate) newList.push(listHarry[index])
+  });
+  return newList;
+}
+
 function renderList(listHarry) {
   let listInsert = "";
   listHarry.map((item, index) => {
@@ -25,7 +44,7 @@ function renderList(listHarry) {
           <li> <img src="${valitate.link}" alt="Picture of ${item.name}"> </li>
           <li>Name: ${item.name}</li>
           <li>Birth: ${item.birth}</li>
-          <li>House: ${item.house ? item.house : "No aplica"}</li>
+          <li>House: ${item.house ? item.house : "Not apply"}</li>
           <li>Species: ${item.species}</li>
           <li>Gender: ${item.gender}</li>
         </ul>`
@@ -33,6 +52,7 @@ function renderList(listHarry) {
   })
   charactersName.innerHTML = listInsert
 }
+
 navToogle.addEventListener('click', () => {
   navMenu.classList.toggle('Menu_visible');
   if (navMenu.classList.contains('Menu_visible')) {
@@ -40,13 +60,73 @@ navToogle.addEventListener('click', () => {
   }else {
     navToogle.setAttribute('aria-label', 'Open menu');
   }
-})
+});
 
+const pCalc = document.querySelector('#p-calculate');
+pCalc.style.display = 'none';
 const selectHouse = document.getElementById("sl-house");
 selectHouse.addEventListener('change', () => {
-  let dataFilter;
-  if(selectHouse.value != "")  dataFilter = dataList.characters.filter((item) => item.house === selectHouse.value)
-  else dataFilter = dataList.characters
-  
-  renderList(dataFilter)
-})
+  dataFilter = dataList.characters
+  if(selectHouse.value !== "All Characters") {
+    dataFilter = filterData(dataFilter, selectHouse.value);
+    const count = listCharacters(dataFilter).length-1
+    pCalc.innerHTML = `Total characters: ${count+1}, Percentage: ${calculate(count+1)}%`
+    pCalc.style.display = 'block'; 
+  }
+  else {
+    pCalc.style.display = 'none';
+  }
+  if(slSort.value !== "sort-by") {
+    dataFilter = sortList(dataFilter)
+  }
+  renderList(dataFilter);
+  title.style.display='none';
+  subtitle.style.display = 'block';
+  subtitle.textContent = `${selectHouse.value}`;
+});
+
+const btnHome = document.querySelector('#btn-home');
+const title = document.querySelector('h1');
+const subtitle = document.querySelector('#subtitle');
+const protagonists = [
+  'Harry Potter', 
+  'Ronald Weasley', 
+  'Hermione Granger', 
+  'Albus Dumbledore', 
+  'Severus Snape', 
+  'Tom Riddle (Voldemort)'
+];
+
+const renderProtagonists = () => {
+  dataFilter = filterProtagonists(dataList.characters, protagonists);
+  pCalc.style.display = 'none';
+  title.style.display = 'block';
+  selectHouse.value = 'Filter by';
+  slSort.value = 'sort-by';
+  subtitle.style.display = 'none';
+  renderList(dataFilter);
+};
+
+btnHome.addEventListener('click', () => {
+  renderProtagonists();
+});
+
+slSort.addEventListener('change', () => {
+  dataFilter = dataList.characters
+  if(slSort.value !== "sort-by") {
+    title.style.display = 'none';
+    pCalc.style.display = 'none';
+    selectHouse.value='All Characters';
+    renderList(sortList(dataFilter));
+  }else {
+    selectHouse.value='Filter by';
+    renderProtagonists();
+  }
+});
+
+const searchBar = document.querySelector('#search');
+searchBar.addEventListener('keyup', () => {
+  title.style.display = 'none';
+  renderList(searchFilter(dataList.characters, searchBar.value.toLowerCase()));
+  if (searchBar.value === '') renderProtagonists();
+});
